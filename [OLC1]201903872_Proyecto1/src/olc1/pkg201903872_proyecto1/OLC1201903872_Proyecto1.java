70a21fbbcd59d5fc4550d750acf5a12300fdf9cd
Rudy Aarón Gopal Marroquín Garcia <3075624920403@ingenieria.usac.edu.gt>
2021-02-16 12:13:28 -0600

    Creación del Proyecto

---
+++ [OLC1]201903872_Proyecto1/src/olc1/pkg201903872_proyecto1/OLC1201903872_Proyecto1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olc1.pkg201903872_proyecto1;
+
+/**
+ *
+ * @author Marro
+ */
+public class OLC1201903872_Proyecto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
