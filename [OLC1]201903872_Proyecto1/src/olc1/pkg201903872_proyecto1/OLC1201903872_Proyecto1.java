2c34b18f2c23750f62fd327ca606f48e4a8d3f54
Rudy Aarón Gopal Marroquín Garcia <3075624920403@ingenieria.usac.edu.gt>
2021-02-21 13:07:39 -0600

    ANALIZADORES V1.0
    PRIMERA VERSIONES DEL ANALIZADOR LEXICO Y SINTACTICO

--- [OLC1]201903872_Proyecto1/src/olc1/pkg201903872_proyecto1/OLC1201903872_Proyecto1.java
+++ [OLC1]201903872_Proyecto1/src/olc1/pkg201903872_proyecto1/OLC1201903872_Proyecto1.java
@@ -5,6 +5,8 @@
  */
 package olc1.pkg201903872_proyecto1;
 
+import java.io.FileInputStream;
+
 /**
  *
  * @author Marro
@@ -15,7 +17,18 @@ public class OLC1201903872_Proyecto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here;
+        interpretar("entrada.txt");
     }
     
+    private static void interpretar(String path) {
+        analizadores.Sintactico pars;
+        try {
+            pars=new analizadores.Sintactico(new analizadores.Lexico(new FileInputStream(path)));
+            pars.parse();        
+        } catch (Exception ex) {
+            System.out.println("Error fatal en compilación de entrada.");
+            System.out.println("Causa: "+ex.getCause());
+        } 
+    }
 }
